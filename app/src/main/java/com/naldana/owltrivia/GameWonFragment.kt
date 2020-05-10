1bb7d01861864ed35e039ccf8f712d4ca204c6e4
00400815@uca.edu.sv <16137238>
2020-05-10 01:25:37 -0600

    applying intents with actions send.

--- app/src/main/java/com/naldana/owltrivia/GameWonFragment.kt
+++ app/src/main/java/com/naldana/owltrivia/GameWonFragment.kt
@@ -1,6 +1,8 @@
 package com.naldana.owltrivia
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +10,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import com.google.android.material.snackbar.Snackbar
 import com.naldana.owltrivia.databinding.FragmentGameWonBinding
 
 /**
@@ -29,8 +32,28 @@ class GameWonFragment : Fragment() {
                 .navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
         }
         val args = GameWonFragmentArgs.fromBundle(arguments!!)
-        Toast.makeText(context, "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}", Toast.LENGTH_SHORT).show()
+        //Toast.makeText(context, "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}", Toast.LENGTH_SHORT).show()
+        Snackbar.make((container as View),
+            "NumCorrect: ${args.numCorrect}," +
+                    " NumQuestions: ${args.numQuestions}",
+            Snackbar.LENGTH_LONG).setAction("OK"){
+            Log.d("GameWonFragment","SnackBar")
+        }.show()
+
+        setHasOptionsMenu(true)
         return binding.root
     }
 
+    private fun getShareIntent(): Intent {
+        val args = GameWonFragmentArgs.fromBundle(arguments!!)
+        //val shareIntent = Intent(Intent.ACTION_SEND)
+        //shareIntent.setType("text/plain").putExtra(Intent.EXTRA_TEXT,"NumCorrect: ${args.numCorrect}," +
+                //" NumQuestions: ${args.numQuestions}")
+        //return shareIntent
+        return Intent(Intent(Intent.ACTION_SEND)).apply {
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT," NumQuestions: ${args.numQuestions}"+
+                    " NumQuestions: ${args.numQuestions}")
+        }
+    }
 }
